a4651ec436f84755dc9eea1f0ecb73d01c679a9c
Losko <daca.loris@gmail.com>
2023-03-10 16:45:48 +0100

    front nav/footer (html) + controller home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Entity\Author;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController
+{
+     #[route(path: "/", name: "home")]
+     public function home (string $name = null): Response
+     {
+         return $this->render('base.html.twig', []);
+     }
+}
