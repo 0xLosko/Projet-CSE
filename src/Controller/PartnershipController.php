0c32858175ec874d4109f072777f55da4904e7dd
Tdcrq <tducrocq2612@gmail.com>
2023-03-12 18:01:26 +0100

    path about

--- src/Controller/PartnershipController.php
+++ src/Controller/PartnershipController.php
@@ -12,7 +12,6 @@ class PartnershipController extends AbstractController
     public function index(): Response
     {
         return $this->render('partnership/index.html.twig', [
-            'controller_name' => 'PartnershipController',
         ]);
     }
 }
