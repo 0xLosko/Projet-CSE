be9dd1f8bae020a05f0cfc89b93c74ad735fba40
Tdcrq <tducrocq2612@gmail.com>
2023-03-12 21:46:50 +0100

    path ticketing

---
+++ src/Controller/TicketingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TicketingController extends AbstractController
+{
+    #[Route('/ticketing', name: 'ticketing')]
+    public function index(): Response
+    {
+        return $this->render('ticketing/index.html.twig', [
+        ]);
+    }
+}
