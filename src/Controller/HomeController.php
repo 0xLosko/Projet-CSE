d4cae2009266ed9275c822b3f718e0cd912e9b29
Losko <daca.loris@gmail.com>
2023-03-14 13:38:10 +0100

    functionality rdm Partners in sidebar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,16 +7,14 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Article;
-use App\Entity\Author;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
-     #[route(path: "/", name: "home")]
-     public function home (string $name = null): Response
+     #[route(path: "/d", name: "home")]
+     public function home (): Response
      {
          return $this->render('home/index.html.twig', [
          ]);
