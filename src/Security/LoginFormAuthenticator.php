a5b331867e923c090862002229653b3f6c69140f
Losko <daca.loris@gmail.com>
2023-03-20 17:07:10 +0100

    fix auth

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -47,7 +47,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_admin'));
+        return new RedirectResponse($this->urlGenerator->generate('about'));
     }
 
     protected function getLoginUrl(Request $request): string
