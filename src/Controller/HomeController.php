58003a38e61cc1a58a1e47ede56c90b15ed3cc8c
Losko <daca.loris@gmail.com>
2023-03-20 20:34:36 +0100

    Functionality BO/Modify home content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\ContentPage;
+use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Page;
 class HomeController extends AbstractController
 {
     #[route(path: "/", name: "home")]
-    public function home (): Response
+    public function home (ManagerRegistry $em): Response
     {
-        return $this->render('home/index.html.twig');
+        $currentPage = $em->getRepository(Page::class)->findOneBy(['namePage' => 'Accueil']);
+        $homeContent = $em->getRepository(ContentPage::class)->findOneBy(['page' => $currentPage])->getTextContent();
+
+        return $this->render('home/index.html.twig', [
+            'homeContent' => $homeContent,
+        ]);
     }
 }
