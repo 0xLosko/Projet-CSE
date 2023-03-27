c2172457b2d849a1fa9ed10ebe6711788d48fb87
Losko <daca.loris@gmail.com>
2023-03-27 15:51:15 +0200

    Responsive navbar

--- public/js/nav-resp.js
+++ public/js/nav-resp.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     let toggleBtn = document.querySelector('.brg');
     let x = document.querySelector('.nav-link-con');
+    let navCon = document.querySelector('.nav-con');
     let imgCon = document.querySelector('.nav-img-con');
     function toggle() {
-        x.style.display = (x.style.display === "flex") ? "none" : "flex";
+        if(x.style.display === "flex"){
+            x.style.display = "none";
+        } else {
+            x.style.display = "flex";
+            navCon.style.paddingLeft = '2%';
+        }
         if (imgCon.style.display === "none") {
             imgCon.style.display = "flex";
         } else {
