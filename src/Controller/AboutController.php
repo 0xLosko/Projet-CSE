64940f68b43395dd6b37efdbd6fd60dd68f200e0
tducrocq <90860668+Tdcrq@users.noreply.github.com>
2023-03-27 16:13:20 +0200

    #09 view about (#50)
    * .
    
    * .
    
    * js show/hide actions
    
    * add entity member
    
    * show members
    
    * scrollbar style
    
    * clear members code

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\ContentPage;
+use App\Entity\File;
+use App\Entity\Member;
+use App\Entity\Page;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +14,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class AboutController extends AbstractController
 {
     #[Route('/a-propos', name: 'about')]
-    public function index(): Response
+    public function about(ManagerRegistry $em): Response
     {
-        return $this->render('about/index.html.twig');
+        $members = $em->getRepository(Member::class)->findAll();
+        foreach ($members as $member) {
+            $member->setFile($em->getRepository(File::class)->findOneBy(['id' => $member->getFile()->getId()]));
+        }
+
+        $currentPage = $em->getRepository(Page::class)->findOneBy(['namePage' => 'about']);
+        $aboutActions = $em->getRepository(ContentPage::class)->findOneBy(
+            array('page' => $currentPage, 'positionPage' => 'actions')
+        );
+        $aboutRule = $em->getRepository(ContentPage::class)->findOneBy(
+            array('page' => $currentPage, 'positionPage' => 'rule')
+        );
+        $aboutAdditionalInfo = $em->getRepository(ContentPage::class)->findOneBy(
+            array('page' => $currentPage, 'positionPage' => 'additional-info')
+        );
+
+        $actionsLines = explode("\n", $aboutActions->getTextContent());
+        $actionsLines = array_diff($actionsLines, [""]);
+
+        $addInfo = explode("\n", $aboutAdditionalInfo->getTextContent());
+        $addInfo = array_diff($addInfo, [""]);
+
+
+        return $this->render('about/index.html.twig', [
+            'members' => $members,
+            'actions' => $actionsLines,
+            'rule' => $aboutRule,
+            'additional_info' => $addInfo,
+        ]);
     }
 }
---
+++ src/Entity/Member.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MemberRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MemberRepository::class)]
+class Member
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\OneToOne(inversedBy: 'member', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?File $file = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function setFile(File $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+}
