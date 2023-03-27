60ac1025a8edf10e607552db5dbf725619b1e71a
Tdcrq <tducrocq2612@gmail.com>
2023-03-27 11:35:44 +0200

    js show/hide actions

--- public/js/about-actions.js
+++ public/js/about-actions.js
@@ -1,9 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let btnShow = document.querySelector(".show-act");
+    let btnHide = document.querySelector(".hide-act");
     let actions = document.querySelector(".actions").children;
-    console.log(actions);
+
     for (let index = 3; index < actions.length; index++) {
         const element = actions[index];
         element.style.display = "none";
     }
-    // btn view more
+    btnHide.style.display = "none";
+
+    function show() {        
+        for (let index = 3; index < actions.length; index++) {
+            const element = actions[index];
+            element.style.display = "flex";
+        }
+        btnHide.style.display = "flex";
+        btnShow.style.display = "none";
+    }
+
+    function hide() {        
+        for (let index = 3; index < actions.length; index++) {
+            const element = actions[index];
+            element.style.display = "none";
+        }
+        btnHide.style.display = "none";
+        btnShow.style.display = "flex";
+    }
+
+    btnShow.addEventListener("click", show);
+    btnHide.addEventListener("click", hide);
 });
